b8f829a47ba56c1bab1cb39c3481c018c482f4a6
Ilya Voronin <voroninilya115@gmail.com>
2023-03-10 00:03:36 +0400

    fix 4

--- 4.cpp
+++ 4.cpp
@@ -3,33 +3,48 @@
 #include <iomanip>
 #include <chrono>
 
-
-float pairwise_sum_simd(float* x, float *y, int n) {
-    if (n == 1) {
-        return x[0];
-    } else if (n == 2) {
-        return x[0] + x[1];
-    } else if (n == 3) {
-        return x[0] + x[1] + x[2];
+float pairwise_sum_simd_arr(float* x, float *y, int n) {
+    if (n <= 8) {
+        float sum = 0;
+        for (int i = 0; i < n; i++) {
+            sum += x[i];
+        }
+        return sum;
     }
 
     #pragma omp simd
-    for (int i = 0; i < n - 3; i += 4) {
-        y[i / 2] = x[i] + x[i + 1];
-        y[i / 2 + 1] = x[i + 2] + x[i + 3];
+    for (int i = 0; i < n; i += 8) {
+        y[i / 2 + 0] = x[i + 0] + x[i + 4];
+        y[i / 2 + 1] = x[i + 1] + x[i + 5];
+        y[i / 2 + 2] = x[i + 2] + x[i + 6];
+        y[i / 2 + 3] = x[i + 3] + x[i + 7];
     }
 
-    int i = n - n % 4;
-    if (n % 4 == 1) {
-        y[i / 2] = x[i];
-    } else if (n % 4 == 2) {
-        y[i / 2] = x[i] + x[i + 1];
-    } else if (n % 4 == 3) {
-        y[i / 2] = x[i] + x[i + 1];
-        y[i / 2 + 1] = x[i + 2];
+    int new_size = (n - n % 8) / 2;
+    if (n % 8 < 4) {
+        new_size += n % 4;
+    } else {
+        new_size += 4;
+    }
+    while (new_size % 8 != 0) {
+        y[new_size] = 0;
+        new_size++;
     }
 
-    return pairwise_sum_simd(y, x, (n - 1) / 2 + 1);
+
+    return pairwise_sum_simd_arr(y, x, new_size);
+}
+
+void prepare(std::vector<float>& x) {
+    if (x.size() % 8 != 0) {
+        x.resize((x.size() / 8 + 1) * 8);
+    }
+}
+
+float pairwise_sum_simd(std::vector<float> &x, std::vector<float> &y, int n) {
+    prepare(x);
+    prepare(y);
+    return pairwise_sum_simd_arr(x.data(), y.data(), n);
 }
 
 float simple_sum(const float* x, int n) {
@@ -48,8 +63,8 @@ int main() {
         x[i] = 50 + 1.0f / 3;
     }
 
-    std::cout << std::setprecision(10) << simple_sum(x.data(), n) << " " << pairwise_sum_simd(x.data(), y.data(), n) << std::endl;
-    // simple: 51541.01953 pairwise: 51541.33203 correct: 51541 + 1/3
+    std::cout << std::setprecision(10) << simple_sum(x.data(), n) << " " << pairwise_sum_simd(x, y, n) << std::endl;
+    // simple: 51541.01953 pairwise: 51541.33594 correct: 51541 + 1/3
 
     n = 16 * 1024 * 1024;
     x.resize(n);
@@ -63,7 +78,7 @@ int main() {
     auto simple_time = (std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::steady_clock::now() - start).count());
 
     start = std::chrono::steady_clock::now();
-    float pairwise_res = pairwise_sum_simd(x.data(), y.data(), n);
+    float pairwise_res = pairwise_sum_simd(x, y, n);
     auto pairwise_time = (std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::steady_clock::now() - start).count());
     std::cout << simple_res << " " << pairwise_res << std::endl;
     std::cout << simple_time << " " << pairwise_time << std::endl;
